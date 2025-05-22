26a7c1a0e8323ba7b6706d9b085c9057edf067f9
Giulia Piombo <giulia.piombo01@gmail.com>
2025-05-22 15:09:03 -0500

    feat: cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from agents import (
     translate_text,
@@ -8,6 +9,14 @@ from agents import (
 )
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Vite dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # --- Request Models ---
 
