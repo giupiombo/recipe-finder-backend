00109e7c9b483d01a6eb01e652ad9c1d1bc6320c
Giulia Piombo <giulia.piombo01@gmail.com>
2025-05-30 15:47:56 -0500

    feat: application domain

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite dev server
+    allow_origins=["http://localhost:5173", "https://recipe-finder-genai.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
